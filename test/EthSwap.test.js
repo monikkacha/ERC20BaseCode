6e444a8dcb988d1f0986059f99b863e9dc7c5345
monik kacha <monikkacha@moniks-MacBook-Air.local>
2021-12-19 18:08:17 +0530

    cheat sheet

---
+++ test/EthSwap.test.js
@@ -0,0 +1,33 @@
+const { assert } = require('chai')
+const { default: Web3 } = require('web3')
+
+const Token = artifacts.require('Token')
+const EthSwap = artifacts.require('EthSwap')
+
+require('chai').use(require('chai-as-promised')).should()
+
+function tokens (n) {
+    return web3.utils.toWei(n , 'ether')
+}
+
+contract ('EthSwap' , (accounts) => {
+    let token , ethSwap
+
+    before(async () => {
+        token = await Token.new()
+        ethSwap = await EthSwap.new()
+
+        await token.transfer(ethSwap.address , tokens('1000000'))
+    })
+
+    describe('EthSwap deployment' , async () => {
+        it ('contract has a name' , async () => {
+            const name = await ethSwap.name()
+            assert.equal(name , 'EthSwap Instant Exchange')
+        })
+        it ('contract has tokens' , async () => {
+            let balance = await token.balanceOf(ethSwap.address)
+            assert.equal(balance.toString() , tokens('1000000'))
+        })  
+    })
+}) 
